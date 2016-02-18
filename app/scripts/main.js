aab4687152817dec7c5954c1b57990709d95a595
mpkpluft <mpkluft@yandex.ru>
2016-02-18 17:10:02 +0300

    fonts scss images 19.02.1987

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+console.log('\'Allo \'Allo!');
+// partials/owl.carousel.js
+$(".carousel").owlCarousel({
+  items: 1,
+  autoPlay: 4000,
+  lazyLoad: true,
+  singleItem: true,
+});
+
+$(".carousel__chapter").css('height', $(window).height()-$(".main-header").height()-$(".main-footer").height()-$(".main-nav").height()-80);
+
+$(window).resize(function(){
+
+  var windowHeight = $(window).height(),
+      hederHeight = $(".main-header").height(),
+      footerHeight = $(".main-footer").height()+50,
+      navHeight = $(".main-nav").height();
+  $(".carousel__chapter").css('height', windowHeight-hederHeight-footerHeight-navHeight);
+});
