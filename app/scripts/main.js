44c968598ac2f5e116884d9c9c8f633a7a695ac0
mpkpluft <mpkluft@yandex.ru>
2016-02-19 17:31:36 +0300

    nothing 19.02.2016

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,17 @@
 console.log('\'Allo \'Allo!');
 // partials/owl.carousel.js
-$(".carousel").owlCarousel({
-  items: 1,
-  autoPlay: 4000,
-  lazyLoad: true,
-  singleItem: true,
-});
 
-$(".carousel__chapter").css('height', $(window).height()-$(".main-header").height()-$(".main-footer").height()-$(".main-nav").height()-80);
 
-$(window).resize(function(){
+$(function(){
 
-  var windowHeight = $(window).height(),
-      hederHeight = $(".main-header").height(),
-      footerHeight = $(".main-footer").height()+50,
-      navHeight = $(".main-nav").height();
-  $(".carousel__chapter").css('height', windowHeight-hederHeight-footerHeight-navHeight);
-});
+	$(".carousel").owlCarousel({
+	  items: 1,
+	  autoPlay: 4000,
+	  lazyLoad: true,
+	  singleItem: true,
+	});
+
+  $('.nav__ul').smartmenus();
+
+})
+
