6ceab605a7c4cb092258b0f9b3e33821f1e5e570
mpkpluft <mpkluft@yandex.ru>
2016-03-03 18:06:42 +0300

    +animate.css +jquery menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,6 +15,39 @@ $(".burger").click(function(){
 	$(this).toggleClass("is-active");
 });
 
+$(".sub-ul__nav").css("display", "none");
+
+var allLi = $(".li__nav"),
+		ulNav = $(".ul__nav"),
+		allA  = $(".a__nav"),
+		allSubA = $(".sub-a__nav");
+
+allLi
+	.has(".ul__nav")
+	.find(".a__nav")
+	.append("<i class = 'fa fa-sort-desc'></i>");
+
+allA.eq(0).addClass("myMenu-current");
+
+allA.hover(
+	function(){
+		$(this).parent().find(".sub-ul__nav").css("display", "block");
+	},
+	function(){
+		$(this).parent().find(".sub-ul__nav").delay(1000).css("display", "none");
+	}
+);
+
+allSubA.hover(
+	function(){
+		$(this).parent().parent().css("display", "block");
+	},
+	function(){
+		$(this).parent().parent().delay(2000).css("display", "none");
+	}
+);
+
+
 
 })
 
